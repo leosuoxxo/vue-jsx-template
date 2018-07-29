66a7d2af0f5c243296426e297dafa8dc486f1f43
leosuoxxo <leo.suoxxo@gmail.com>
2018-07-29 16:05:15 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,22 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/prettier'
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: true
+      }
+    ]
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script>
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class About extends Vue {
+  @Prop() name = '123';
+  render() {
+    return (
+      <div class="about">
+        <h1>This is an about page123</h1>
+        <span>{this.name}</span>
+      </div>
+    );
+  }
+}
+</script>
+<style lang="scss" scoped>
+.aaa {
+  position: absolute;
+  padding: 30px;
+  &:hover {
+    color: #fff;
+  }
+}
+</style>
